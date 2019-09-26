38ede90c06cec46c230c31c5642e05cfd79ab784
edgelord9ooo <avery_tan@hotmail.com>
2019-09-26 00:28:51 -0600

    first commit. Placeholder set for regions as preliminary map

---
+++ Assets/Scripts/controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("this is controller checking in");
+
+    }
+
+
+    private void OnMouseDown() {
+        Debug.Log("planning phase skipped");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/gameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameController : MonoBehaviour
+{
+    public GameObject obj;
+    public Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        WestorosPhase();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlanningPhase();
+        ActionPhase();
+    }
+
+    void WestorosPhase()
+    {
+        Debug.Log("Westeros Phase INitiateing");
+    }
+
+    void PlanningPhase()
+    {
+        Debug.Log("Planning Phase initiating");
+    }
+    void ActionPhase()
+    {
+        Debug.Log("Action Phase Initianting");
+    }
+}
